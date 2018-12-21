217db50bb260d866432aa756f9a42acfe07cf395
FolhaSeca <42450120+FolhaSeca@users.noreply.github.com>
2018-12-20 22:02:00 -0200

    Add files via upload

---
+++ musica2.py
@@ -0,0 +1,407 @@
+import playsound
+
+def TodasAsMusicas():
+    LegendsNeverDie()
+    Radioactive()
+    Warriors()
+    HearMeNow()
+    Believer()
+    Rise()
+    FlyWithMe()
+
+def LegendsNeverDie():
+    print('Você está ouvindo Legends Never Die.')
+    print('''
+        Legends never die when the world is calling you
+        Can you hear them screaming out your name?
+        Legends never die they become a part of you
+        Every time you bleed for reaching greatness
+        Relentless you survive
+        
+        They never lose hope when everything's cold and the,
+        fighting's near
+        It's deep in their bones they’ll run into smoke when the fire
+        is fierce
+        Oh, pick yourself up ‘cause
+        
+        Legends never die when the world is calling you
+        Can you hear them screaming out your name?
+        Legends never die they become a part of you
+        Every time you bleed for reaching greatness
+        Legends never die
+        
+        They're written down in eternity
+        But you'll never see the price it costs, the scars collected
+        all their lives
+        
+        When everything’s lost, they pick up their hearts and
+        avenge defeat
+        Before it all starts, they suffer through harm just to touch a
+        dream
+        Oh, pick yourself up ‘cause
+        
+        Legends never die when the world is calling you
+        Can you hear them screaming out your name?
+        Legends never die they become a part of you
+        Every time you bleed for reaching greatness
+        Legends never die
+        
+        When the world is calling out your name
+        Begging you to fight
+        Pick yourself up once more
+        Oh, pick yourself up ‘cause
+        
+        Legends never die when the world is calling you
+        Can you hear them screaming out your name?
+        Legends never die they become a part of you
+        Every time you bleed for reaching greatness
+        Legends never die''')
+    playsound.playsound('./LegendsNeverDie.mp3')
+    print()
+
+def Radioactive():
+    print('Você está ouvindo Radioactive.')
+    print('''
+        Oh, oh, oh
+        Oh, oh, oh
+        Oh, oh, oh
+        Oh
+        
+        I’m waking up to ash and dust
+        I wipe my brow and sweat my rust
+        I’m breathing in the chemicals
+        
+        I’m breaking in, shaping up
+        Then checking out on the prison bus
+        This is it, the apocalypse
+        
+        Oh, oh
+        
+        I’m waking up
+        I feel it in my bones
+        Enough to make my system blow
+        
+        Welcome to the new age, to the new age
+        Welcome to the new age, to the new age
+        
+        Oh, oh, oh, oh, oh
+        Oh, oh, oh, oh
+        I'm radioactive, radioactive
+        
+        Oh, oh, oh, oh, oh
+        Oh, oh, oh, oh
+        I'm radioactive, radioactive
+        
+        I raise my flag, and dye my clothes
+        It’s a revolution, I suppose
+        We’re painted red to fit right in
+        
+        Oh, oh
+        
+        I’m breaking in, shaping up
+        Then checking out on the prison bus
+        This is it, the apocalypse
+        
+        Oh, oh
+        
+        I’m waking up
+        I feel it in my bones
+        Enough to make my system blow
+        
+        Welcome to the new age, to the new age
+        Welcome to the new age, to the new age
+        
+        Oh, oh, oh, oh, oh
+        Oh, oh, oh, oh
+        I'm radioactive, radioactive
+        
+        Oh, oh, oh, oh, oh
+        Oh, oh, oh, oh
+        I'm radioactive, radioactive
+        
+        All systems go
+        The sun hasn’t died
+        Deep in my bones
+        Straight from inside
+        
+        I’m waking up
+        I feel it in my bones
+        Enough to make my system blow
+        
+        Welcome to the new age, to the new age
+        Welcome to the new age, to the new age
+        
+        Oh, oh, oh, oh, oh
+        Oh, oh, oh, oh
+        I'm radioactive, radioactive
+        
+        Oh, oh, oh, oh, oh
+        Oh, oh, oh, oh
+        I'm radioactive, radioactive''')
+    playsound.playsound('./Radioactive.mp3')
+    print()
+
+def Warriors():
+    print('Você está ouvindo Warriors.')
+    print('''
+        As a child you would wait
+        And watch from far away
+        But you always knew that you'd be the one
+        That work while they all play
+        
+        In youth you'd lay
+        Awake at night and scheme
+        Of all the things that you would change
+        But it was just a dream!
+        
+        Here we are, don't turn away now
+        We are the warriors that build this town
+        Here we are, don't turn away now
+        We are the warriors that build this town
+        From dust
+        
+        The time will come
+        When you'll have to rise
+        Above the best and prove yourself
+        Your spirit never dies!
+        
+        Farewell, I've gone to take my throne above
+        But don't weep for me
+        'Cause this will be
+        The labor of my love
+        
+        Here we are, don't turn away now
+        We are the warriors that build this town
+        Here we are, don't turn away now
+        We are the warriors that build this town
+        From dust
+        
+        Here we are, don't turn away now
+        We are the warriors that build this town
+        Here we are, don't turn away now
+        We are the warriors that build this town
+        From dust''')
+    playsound.playsound('./Warriors.mp3')
+    print()
+
+def Believer():
+    print('Você está ouvindo Believer.')
+    print('''
+        First things first
+        I'ma say all the words inside my head
+        I'm fired up and tired of
+        the way that things have been, oh-ooh
+        The way that things have been, oh-ooh
+        
+        Second thing second
+        Don't you tell me what you think that I could be
+        I'm the one at the sail,
+        I'm the master of my sea, oh-ooh
+        The master of my sea, oh-ooh
+        
+        I was broken from a young age
+        Taking my sulking to the masses
+        Writing my poems for the few
+        That looked at me took to me,
+        shook to me, feeling me
+        Singing from heartache from the pain
+        Take up my message from the veins
+        Speaking my lesson from the brain
+        Seeing the beauty through the
+        
+        Pain!
+        You made me a,
+        you made me a believer, believer
+        Pain!
+        You break me down,
+        you build me up, believer, believer
+        Pain!
+        I let the bullets fly, oh let them rain
+        My life, my love, my drive, they came from
+        Pain!
+        You made me a,
+        you made me a believer, believer
+        
+        Third things third
+        Send a prayer to the ones up above
+        All the hate that you've heard
+        Has turned your spirit to a dove, oh-ooh
+        Your spirit up above, oh-ooh
+        
+        I was choking in the crowd
+        Building my rain up in the clouds
+        Falling like ashes to the ground
+        Hoping my feelings, they would drown
+        But they never did, ever lived,
+        ebbing and flowing inhibited, limited
+        Till it broke open and it rained down
+        It rained down, like
+        
+        Pain!
+        You made me a,
+        you made me a believer, believer
+        Pain!
+        You break me down,
+        you build me up, believer, believer
+        Pain!
+        I let the bullets fly, oh let them rain
+        My life, my love, my drive, they came from
+        Pain!
+        You made me a,
+        you made me a believer, believer
+        
+        Last things last
+        By the grace of the fire and the flames
+        You're the face of the future,
+        The blood in my veins, oh-ooh
+        The blood in my veins, oh-ooh
+        But they never did, ever lived,
+        ebbing and flowing inhibited, limited
+        Till it broke open and it rained down
+        It rained down, like
+        
+        Pain!
+        You made me a, you made me a believer, believer
+        Pain!
+        You break me down,
+        you build me up, believer, believer
+        Pain!
+        I let the bullets fly, oh let them rain
+        My life, my love, my drive, they came from
+        Pain!
+        You made me a,
+        you made me a believer, believer''')
+    playsound.playsound('./Believer.mp3')
+    print()
+
+def Rise():
+    print('Você está ouvindo Rise.')
+    print('''
+        Welcome to the wild, no heroes and villains
+        Welcome to the war we've only begun, so
+        Pick up your weapon and face it
+        There's blood on the crown go and take it
+        You get one shot to make it out alive, so
+        Higher and higher you chase it
+        It’s deep in your bones, go and take it
+        This is your moment, now is your time, so
+        
+        Prove yourself and
+        Rise, rise
+        Make 'em remember you
+        Rise
+        Push through hell and
+        Rise, rise
+        They will remember you
+        Rise
+        
+        Welcome to the climb up, reach for the summit
+        Visions pray that one false step lead the end, so
+        Higher and higher you chase it
+        It's deep in your blood, go and take it
+        This is your moment, take to the skies, go
+        
+        Prove yourself and
+        Rise, rise
+        Make 'em remember you
+        Rise
+        Push through hell and
+        Rise, rise
+        They will remember you
+        Rise
+        
+        So get along, so get along, go
+        Get along 'long get a move on up
+        So get along, so get along, go
+        Get along 'long get a move on up
+        And as you fight among the death
+        Beneath the dirt
+        Do you know yet?
+        Do you want it?
+        And when the giants call
+        To ask you what you're worth
+        Do you know if
+        Win or die, you'll
+        
+        Prove yourself and
+        Rise, rise
+        Make 'em remember you
+        Rise
+        Push through hell and
+        Rise, rise
+        They will remember you
+        Rise
+        Prove yourself and rise, rise
+        (Pick up your weapon and face it)
+        Rise
+        (It’s deep in your blood, go and take it)
+        Rise, rise
+        (Higher and higher you chase it)
+        Rise, rise''')
+    playsound.playsound('./Rise.mp3')
+    print()
+
+def FlyWithMe():
+    print('Você está ouvindo Fly With Me.')
+    print('''
+        Bu zhi ri luo yue sheng duo shao ge xia qiu
+        Bu zhi wo yi zhe yang ben pao le duo jiu
+        Wo cong chu sheng jiu zhu ding yi sheng de xun qiu
+        Yuan fang na wan mei shi jie de ai he zi you
+        
+        Fly with me
+        In the perfect world
+        Go with me just like a bird
+        Mei shen me neng zu lan zi you de tian di
+        
+        Zhe yang chen mo ai ni bu zhi you duo jiu
+        Wo yuan fu chu wo de sheng ming he suo you
+        Wo yao ni bu gu yi qie gen wo zou
+        Qu xiang na wan mei shi jie de ai he zi you
+        Fly with me
+        In the perfect world
+        Go with me just like a bird
+        Mei shen me neng zu lan wo men zai yi qi
+        
+        Fly with me
+        In the perfect world
+        Go with me just like a bird
+        Mei shen me zu lan zi you de tian di
+        
+        Fly with me
+        In the perfect world
+        Go with me just like a bird
+        Mei shen me neng zu lan wo men zai yi qi
+        Fly with me''')
+    playsound.playsound('./FlyWithMe.mp3')
+
+while True:
+    print('''Qual múscia você gostaria de ouvir:
+     [ 0 ] Todas as musicas
+     [ 1 ] Legends Never Die
+     [ 2 ] Radioactive
+     [ 3 ] Warriors
+     [ 4 ] Believer
+     [ 5 ] Rise
+     [ 6 ] Fly With Me
+     [999] PARA SAIR''')
+    opcao = int(input('Escolha uma das opções acima: '))
+    if opcao == 0:
+        TodasAsMusicas()
+    elif opcao == 1:
+        LegendsNeverDie()
+    elif opcao == 2:
+        Radioactive()
+    elif opcao == 3:
+        Warriors()
+    elif opcao == 4:
+        Believer()
+    elif opcao == 5:
+        Rise()
+    elif opcao == 6:
+        FlyWithMe()
+    elif opcao == 999:
+        break
+    else:
+        print('Opção invalida, tente novamente.')
+
